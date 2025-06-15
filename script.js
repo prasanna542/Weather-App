a9d0bfd453e936dd6e31d16d14580c0d28394996
prasanna <prasanna542002@gmail.com>
2025-06-15 18:15:55 +0530

    updated UI

--- script.js
+++ script.js
@@ -1,74 +1,60 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    //First we capture all the elements here.
-    const city_input = document.getElementById('city_input');
-    const get_weather_btn = document.getElementById('get_weather');
-    const weather_info = document.getElementById('weather_info');
-    const cityname = document.getElementById('cityname');
-    const temp = document.getElementById('temp');
-    const extra_info = document.getElementById('extra_info');
-    const err_msg = document.getElementById('error_msg');
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-messag");
 
-    const API_KEY = "1dd50c33a517ce0ad6b7acfc2d3708e1"; //later we will store such keys in env variables. 
+  const API_KEY = "1dd50c33a517ce0ad6b7acfc2d3708e1"; //env variables
+                    
 
-    err_msg.classList.add('hidden');
-    weather_info.classList.add('hidden');
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
 
-    get_weather_btn.addEventListener("click", async ()=>{
-        err_msg.classList.add('hidden');
-        weather_info.classList.add('hidden');
-        let input_c = city_input.value.trim();
-        if(!input_c){
-            alert("please enter a valid city name");
-            return;    //here empty string is considered as the false value in javascript.
-        }
-        else{
-            try {
-                let weatherData = await getWeatherData(input_c);
-                displayWeatherData(weatherData);
-            } catch (error) {
-                showError();
-            }
+    // it may throw an error
+    // server/database is always in another continent
 
-            
-        }
-    });
-
-
-    async function getWeatherData(cityName){
-        // const url = `https://api.openweathermap.org/data/2.5/onecall?q=${cityName}&appid=${API_KEY}`
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
-        const response = await fetch(url);
-
-        console.log(typeof response);
-        console.log(response);
-
-        if(response.ok){
-            let data = await response.json();
-            console.log(data);
-            
-            return data;
-        }
-        else{
-            // alert("Oops! Something went wrong.");
-            
-            err_msg.classList.remove("hidden");
-            return;
-        }
-        
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
     }
+  });
 
-    function displayWeatherData(weather_data){
-        const name = weather_data.name;
-        const temp2 = weather_data.main.temp;
-        
-        cityname.textContent = name;
-        temp.textContent = temp2;
-        weather_info.classList.remove('hidden');
+  async function fetchWeatherData(city) {
+    //gets the data
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("RESPONSE", response);
+
+    if (!response.ok) {
+      alert("City Not Found.")
+      throw new Error(" City Not found");
     }
+    const data = await response.json();
+    return data;
+  }
 
+  function displayWeatherData(data) {
+    console.log(data);
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
+    descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
 
+    //unlock the display
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
 
-    
-
+  function showError() {
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
 });
